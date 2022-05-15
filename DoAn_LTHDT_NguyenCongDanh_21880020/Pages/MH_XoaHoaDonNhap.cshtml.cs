08b2c0e9b11becde55b32cf63b9212c8d060350a
Danh <nguyencongdanh2410@gmail.com>
2022-05-15 16:19:06 +0700

    xoa hoa don nhap - done

---
+++ DoAn_LTHDT_NguyenCongDanh_21880020/Pages/MH_XoaHoaDonNhap.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Entities;
+using Services;
+
+namespace DoAn_LTHDT_NguyenCongDanh_21880020.Pages.Shared
+{
+    public class MH_XoaHoaDonNhapModel : PageModel
+    {
+        public string chuoi = string.Empty;
+        public HoaDon hdn;
+        IXuLy_HoaDonNhap xuLyHDN;
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+        public MH_XoaHoaDonNhapModel()
+        {
+            xuLyHDN = new XuLy_HoaDonNhap();
+        }
+        public void OnGet()
+        {
+            var kq = xuLyHDN.TimKiemTheoID(Id);
+            if (kq.IsSuccess)
+            {
+                hdn = kq.Data;
+                chuoi = $"Bạn chắc chắn muốn xóa hóa đơn có mã số: {hdn.MaHoaDon}?";
+            }
+            else
+            {
+                chuoi = kq.Message;
+            }
+        }
+        public void OnPost()
+        {
+            var kq = xuLyHDN.XoaHoaDonNhap(Id);
+            if (kq.IsSuccess)
+            {
+                Response.Redirect("/MH_TimKiemHoaDonNhap");
+            }
+            else
+            {
+                chuoi = kq.Message;
+            }
+        }
+    }
+}
